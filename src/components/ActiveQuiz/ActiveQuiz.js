567ad60f02b8a1b2573d7a85e31fdd2b41f5a194
Stanislav Vlasov <vvs00794@gmail.com>
2020-01-12 15:22:21 +0400

    create files Quiz, ActiveQuiz, AnswerList

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import classes from './ActiveQuiz.module.css'
+import AnswerList from './AnswersList/AnswersList'
+
+const ActiveQuiz = props => (
+   <div className = {classes.ActiveQuiz}>
+       <p className = {classes.Question}>
+           <span>
+               <strong>2.</strong>&nbsp;
+               {props.question}
+           </span>
+           <small>4 из 12</small>
+       </p>
+        <AnswerList
+            answers = {props.answers}
+            onAnswerClick = {props.onAnswerClick}
+        />
+   </div>
+)
+
+export default ActiveQuiz
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+import classes from './Quiz.module.css'
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
+class Quiz extends Component {
+    state = {
+        quiz: [
+            {   question: 'Какого цвета небо?',
+                rightAnswerId: 2,
+                answers: [
+                    {text: 'Черное', id: 1},
+                    {text: 'Синее', id: 2},
+                    {text: 'Красное', id: 3},
+                    {text: 'Зеленое', id: 4},
+
+                ]
+            }
+        ],
+
+    }
+
+    onAnswerClickHandler = (answerId)=> {
+        console.log('Answer Id:', answerId)
+    }
+
+    render() {
+        return(
+            <div className = {classes.Quiz}>
+                
+                <div className = {classes.QuizWrapper}>
+                    <h1>Ответьте на все вопросы</h1>
+                    <ActiveQuiz
+                        answers = {this.state.quiz[0].answers}
+                        question = {this.state.quiz[0].question}
+                        onAnswerClick = {this.onAnswerClickHandler}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Quiz
