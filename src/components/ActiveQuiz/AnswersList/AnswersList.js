692d56b821c90b58fc103b4bcba48e35d9876fdd
Stanislav Vlasov <vvs00794@gmail.com>
2020-01-15 21:40:08 +0400

    finished practive forms:

--- src/components/ActiveQuiz/AnswersList/AnswersList.js
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import classes from './AnswersList.module.css'
+import classes from './AnswersList.css'
 import AnswerItem from './AnswerItem/AnswerItem'
 
-const AnswersList = props => (
-    <ul className = {classes.AnswersList}>
-        {props.answers.map((answer, index)=> {
-            return (
-            <AnswerItem 
-            answer = {answer} 
-            key = {index}
-            onAnswerClick = {props.onAnswerClick}
-            state = {props.state ? props.state[answer.id] : null}
-            />
-            )
-        }) }
+const AnswersList = props => {
+  return (
+    <ul className={classes.AnswersList}>
+      { props.answers.map((answer, index) => {
+        return (
+          <AnswerItem
+            key={index}
+            answer={answer}
+            onAnswerClick={props.onAnswerClick}
+            state={props.state ? props.state[answer.id] : null}
+          />
+        )
+      }) }
     </ul>
-)
-
+  )
+}
 
 export default AnswersList
